ab6cb9d3d097c9b3717aaebbf213bc2bcd19cb7b
evanssd18 <evansantwi655@gmail.com>
2026-07-14 01:59:50 +0000

    Update bot name and owner in settings

--- settings.js
+++ settings.js
@@ -1,8 +1,8 @@
 const settings = {
   packname: 'Knight Bot',
   author: '‎',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
+  botName: "Ghost Bot",
+  botOwner: 'Evans', // Your name
   ownerNumber: '233257946555', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
